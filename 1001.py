2482f990afaa1c188c18e6fc8770c5c8f8921971
jungmin9021 <78529167+jungmin9021@users.noreply.github.com>
2021-02-05 20:51:53 +0900

    Update 1001.py

--- 1001.py
+++ 1001.py
@@ -1,8 +1,37 @@
 # 백준 1001 번 문제
 input_data=input().split(' ')
-# '3' '2'
 A = int(input_data[0])
 B = int(input_data[1])
 print(A-B)
 
 # 1000번 문제에서 연산만 다르게 하면 된다. 
+
+# 10998
+input_data=input().split(' ')
+A = int(input_data[0])
+B = int(input_data[1])
+print(A*B)
+
+#10998번 역시 똑같다. 
+
+#1008
+input_data=input().split(' ')
+A = int(input_data[0])
+B = int(input_data[1])
+print(A/B)
+
+#1008번도 같다. 
+
+#10869번
+
+input_data=input().split(' ')
+A = int(input_data[0])
+B = int(input_data[1])
+print(A+B)
+print(A-B)
+print(A*B)
+print(A//B)
+print(A%B)
+
+# 몫을 구할 때는 // 슬래쉬 두 번
+# 나머지를 구할 때는 % 
